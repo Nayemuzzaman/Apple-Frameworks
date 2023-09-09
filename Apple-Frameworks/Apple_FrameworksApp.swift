61ea07d9da0c74dcd739be93695407a5acd297f9
nayemuzzaman <nayemzaman@gmail.com>
2023-09-10 08:26:09 +0900

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Nayemuzzaman on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
